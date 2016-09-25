22947d5fa7ac32494d2e1649e2e6a494d2f807e4
Renzo Crisóstomo <renzo.crisostomo@xing.com>
2016-09-25 15:37:20 +0200

    Initial commit

---
+++ FanSabisu/Sources/ViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
